d8a5826a57d8d4f7ba8ea89b1c73bd809305cc0e
Frosb1 <frosb1official@gmail.com>
2026-04-10 20:44:23 +0700

    feat: implement main page switching logic

--- src/js/currentQuestion.js
+++ src/js/currentQuestion.js
@@ -1,1 +1,36 @@
-let currentQuestion = 0
+import { questions } from "./Questions.js";
+const questionScreen = document.getElementById("questionScreen");
+const finalScreen = document.getElementById("finalScreen");
+const qText = document.getElementById('qText')
+const yesBtn = document.getElementById('yesBtn')
+const noBtn = document.getElementById('noBtn')
+
+let currentQuestion = 0;
+
+function renderQuestion() {
+
+    if (currentQuestion >= questions.length) {
+        showFinalScreen();
+        return;
+    }
+
+    let q = questions[currentQuestion];
+
+    qText.textContent = q.text;
+    yesBtn.textContent = q.btnYes;
+    noBtn.textContent = q.btnNo;
+}
+
+yesBtn.addEventListener('click', () => {
+    currentQuestion++;
+    renderQuestion();
+})
+
+function showFinalScreen() {
+    yesBtn.addEventListener('click', () => {
+        questionScreen.classList.add('hidden')
+        finalScreen.classList.remove('hidden')
+    })
+}
+
+renderQuestion()
