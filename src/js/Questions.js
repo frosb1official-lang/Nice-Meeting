d7acd57f79961fca52b1d6c33c8b4834bc0541ea
Frosb1 <frosb1official@gmail.com>
2026-04-12 18:52:20 +0700

    feat: prepare to deploy

--- src/js/Questions.js
+++ src/js/Questions.js
@@ -4,25 +4,25 @@ export const questions = [
         text: "У меня есть отличная идея...",
         btnYes: "Какая?",
         btnNo: "Не интересно",
-        gif: '/images/peachcat-cat.gif'
+        gif: './images/peachcat-cat.gif'
     },
     {
         id: 2,
         text: "Хочешь провести время вместе?",
         btnYes: "Конечно хочу!",
         btnNo: "Может быть...",
-        gif: '/images/cute.gif'
+        gif: './images/cute.gif'
     },
     {
         id: 3,
         text: "Так что... ты согласна(-ен) сходить со мной на свидание?",
         btnYes: "Да, очень хочу!",
         btnNo: "В другой раз",
-        gif: '/images/mochi-mochimochi.gif'
+        gif: './images/mochi-mochimochi.gif'
     },
     {
         id: 4,
         text: "Супер, я в полной боевой готовности к встрече с тобой!",
-        gif: '/images/mochi-final-screen.gif'
+        gif: './images/mochi-final-screen.gif'
     }
 ];
